5bf3ad1f7595216ac2cf9de260f568e11e9a1ffc
Wolf <a17jul@gmail.com>
2026-03-09 19:55:06 -0600

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img src="{{ asset('logo-lobito-sin-fondo.png') }}" class="h-14 w-auto" alt="Logo Lobito">
+<img src="{{ asset('logo-lobito-sin-fondo.png') }}" {{ $attributes }} alt="Logo Lobito">
 
